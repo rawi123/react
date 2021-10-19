9ce1ef6d51a7bda99596016e0dc2d4f8c18ce462
rawi123 <rawipro.rl@gmail.com>
2021-10-19 11:10:16 +0300

    ex8

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+{
 // import EX22 from './components/ex-2.2/2.2'//run using <Ex22/>
 // import EX31 from './components/ex3/ex3.1/GreenBox';<EX31/>
 // import Quiz from "./components/ex3/ex3.2/Quiz";<Quiz/>
 // import Btns from "./components/ex4/ex4.1/Btns2";<Btns/>
 // import AllCards from "./components/ex4/ex4.2/AllCards";<AllCards/>
 // import AllCards from "./components/ex6.1/AllCards";<AllCards/>
-// import Weather from './components/codeAlong/weather'<Weather/>
+// import Weather from './components/codeAlong/weather'<Weather />
 // import Increcment from './components/ex7/7.1 increcment/increcment';<Increcment/>
 // import BigBox from "./components/ex7/7.2/bigBox";<BigBox/>
+// import Ex81 from "./components/ex8/ex8.1/index"<Ex81/>
+// import Ex82 from "./components/ex8/ex8.2/Ex8.2";<Ex82 />
+// import Ex83 from "./components/ex8/ex8.3/changing-box";<Ex83/>
+}
 
 function App() {
 	return (
